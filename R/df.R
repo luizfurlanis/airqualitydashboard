6ca914eed8fd45a2355aacb5f0745411121a121c
LuizFurlanis <122633468+luizfurlanis@users.noreply.github.com>
2025-01-08 17:35:43 -0300

    dataframe to deploy

--- R/df.R
+++ R/df.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-data <- read.csv("C:/Users/LuizOctavioFurlanis/Documents/GitHub/Luiz Furlanis/roadaccidentdashboard/data/RoadAccident.csv")
+data <- read.csv("data/RoadAccident.csv")
 
 map_df <- data |>
   select(Year, lat = Latitude, long = Longitude)
