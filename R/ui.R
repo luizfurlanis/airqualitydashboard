43c9521933c3b7625dde5e2c017b2d929fd2ac6b
LuizFurlanis <122633468+luizfurlanis@users.noreply.github.com>
2025-01-14 12:49:15 -0300

    [fix] ui sidebar

--- R/ui.R
+++ R/ui.R
@@ -1,22 +1,16 @@
 ui <- page_navbar(
 
   title = "UK Traffic Accidents",
+  sidebar = md.card,
 
   nav_panel(
     title = "View Map",
-    page_sidebar(
-      sidebar = md.card,
-      md.map_UI()
-    )
+    md.map_UI()
   ),
 
   nav_panel(
     title = "Graphics",
-    page_sidebar(
-      sidebar = md.card,
-      md.graphic_UI()
-    )
-
+    md.graphic_UI()
   )
 )
 
