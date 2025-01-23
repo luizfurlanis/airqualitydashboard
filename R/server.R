ebe136bb0edc01d1af35f22ac2ca257598e42b40
LuizFurlanis <122633468+luizfurlanis@users.noreply.github.com>
2025-01-23 13:03:53 -0300

    [style] server and ui call modules

--- R/server.R
+++ R/server.R
@@ -1,10 +1,6 @@
 server <- function(input, output, session) {
 
-  select_year <- reactive({
-    req(input$year)
-    as.numeric(input$year)
-  })
-
   md.map(select_year = select_year, map_df = map_df)
   md.graphic(data_table = df.count_category)
+  md.second_graphics()
 }
